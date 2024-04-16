7a164c6e26cfdfa73a252422208a12f7ec990813
Kumar Harsh <kharsh560@gmail.com>
2024-04-17 00:38:49 +0530

    Attached variables to input fields
    Attached variables to input fields.
    Like numbers, splCharacters and slider are attached to the variable in UseState.
    Also, made the password generator as per sir's fashion, which is using a string array of alphabets, splChars and numbers!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 function App() {
-  const [Length, setLength] = useState(6);
+  const [Length, setLength] = useState(16);
   const [IncludeNumbers, setIncludeNumbers] = useState(false);
   const [IncludeSplChars, setIncludeSplChars] = useState(false);
   const [Password, setPassword] = useState();
 
-  function inRangeNumberGenerator() {}
-
-  function passwordGenerator() {
+  function passwordGeneratorKHstyle() {
     let randObj = {
       randomNum1: () => {
         return Math.floor(Math.random() * 26) + 65;
@@ -22,7 +20,7 @@ function App() {
       // alphaNum: `randomNum${randomNum3}`,
     };
 
-    let alphaNum = randObj[`randomNum${randObj.randomNum3()}`](); 
+    let alphaNum = randObj[`randomNum${randObj.randomNum3()}`]();
     // Of course, I needed to write "()" at those two places bcoz they were functions!!
     // console.log(alphaNum);
     // This alphaNum now contain numbers in this range [65, 90] and [97, 122]
@@ -42,6 +40,24 @@ function App() {
     document.getElementById("passwordBox").innerText = passCode;
   }
 
+  const passwordGenerator = useCallback(() => {
+    let password = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let numbers = "0123456789";
+    let splChars = "!@#$%^&*+_-~`";
+
+    if (IncludeNumbers) string += numbers;
+    if (IncludeSplChars) string += splChars;
+    // If they are not included, then string remains string only!
+
+    for (let i = 0; i <= Length; i++) {
+      password += string[Math.floor(Math.random() * string.length)];
+      //Did not do "+1" bcoz the string array starts from 0th index only!
+    }
+
+    setPassword(password);
+  }, [Length, IncludeNumbers, IncludeSplChars]);
+
   return (
     <>
       <div className=" w-screen h-screen bg-zinc-700 flex justify-center">
@@ -56,7 +72,7 @@ function App() {
               className="w-[80%] h-10 bg-slate-300 rounded-2xl m-3 text-lg font-bold font-ubuntu flex justify-center items-center"
             ></div>
             <button
-              onClick={() => passwordGenerator()}
+              onClick={() => passwordGeneratorKHstyle()}
               className=" bg-green-400 rounded-lg h-fit p-1 hover:bg-green-500 active:text-lg active:ring-4 active:ring-green-800"
             >
               Copy
@@ -65,24 +81,46 @@ function App() {
           <div className="flex justify-center items-center">
             <input
               type="range"
-              min="5"
+              min="1"
               max="100"
+              value={Length}
               className=" cursor-pointer appearance-none rounded-lg h-2 bg-gray-300"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
             ></input>
-            <span className=" font-semibold mx-2 cursor-default">Length()</span>
+            <span className=" font-semibold mx-2 cursor-default">
+              Length({Length})
+            </span>
             <span className=" text-xl text-amber-100/45 cursor-default ">
               {" "}
               |{" "}
             </span>
             <span className=" font-semibold mx-2 cursor-default">Numbers</span>
-            <input type="checkbox" className=" cursor-pointer"></input>
+            {/* Number Checkbox */}
+            <input
+              type="checkbox"
+              className=" cursor-pointer"
+              defaultChecked={IncludeNumbers}
+              onChange={() => {
+                setIncludeNumbers((prev) => !prev);
+              }}
+            ></input>
             <span className=" text-xl text-amber-100/45 ml-2 cursor-default">
               |
             </span>
             <span className=" font-semibold mx-2 cursor-default">
               Spl Characters
             </span>
-            <input type="checkbox" className=" cursor-pointer"></input>
+            {/* SplChars Checkbox */}
+            <input
+              type="checkbox"
+              className=" cursor-pointer"
+              defaultChecked={IncludeSplChars}
+              onChange={() => {
+                setIncludeSplChars((prev) => !prev);
+              }}
+            ></input>
           </div>
         </div>
       </div>
